c9894267e28bcf6f118400ced19d7fa5125c9f2a
user <namsic6460@gmail.com>
2020-05-13 10:24:13 +0900

    Step1 코드 유지보수를 위한 수정

--- cpp1/cpp1/c1.cpp
+++ cpp1/cpp1/c1.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 #define NAME_LEN 20
+#define ACCOUNT_MAX 100
 
 typedef struct {
 	int accId;
@@ -11,7 +12,14 @@ typedef struct {
 	char cusName[NAME_LEN];
 } Account;
 
-Account* accounts = new Account[100];
+typedef enum {
+	MK_ACC = 1,
+	DEPOSIT = 2,
+	WITHDRAW = 3,
+	SEE_LIST = 4
+} DUTY_LIST;
+
+Account* accounts = new Account[ACCOUNT_MAX];
 int accountSize = 0;
 
 int findAccountIdx(int id) {
@@ -23,114 +31,144 @@ int findAccountIdx(int id) {
 	return -1;
 }
 
-void duty(int input) {
-	int id;
-	int find;
+void mkAcc() {
+	char name[NAME_LEN];
+	int balance;
+	int id, find;
 
-	switch (input) {
-	case 1:
-		char name[NAME_LEN];
-		int balance;
+	cout << "계좌ID: ";
+	cin >> id;
+	cout << "이름: ";
+	cin >> name;
+	cout << "입금액: ";
+	cin >> balance;
 
-		cout << "[계좌개설]" << endl;
-		cout << "계좌ID: ";
-		cin >> id;
-		cout << "이름: ";
-		cin >> name;
-		cout << "입금액: ";
-		cin >> balance;
-
-		if (accountSize == 100) {
-			cout << "계좌가 이미 100개입니다." << endl;
-			return;
-		}
-
-		find = findAccountIdx(id);
-		if (find != -1) {
-			cout << "이미 동일한 계좌 번호를 가진 계좌가 존재합니다." << endl;
-			return;
-		}
-
-		if (balance < 10) {
-			cout << "계좌 생성 시 최소 10원 이상의 돈을 입금해야합니다." << endl;
-			return;
-		}
-
-		accounts[accountSize].accId = id;
-		accounts[accountSize].balance = balance;
-		strcpy(accounts[accountSize++].cusName, name);
-
-		return;
-	case 2:
-		cout << "[입    금]" << endl;
-		cout << "계좌ID: ";
-		cin >> id;
-		cout << "입금액: ";
-		cin >> balance;
-
-		if (accountSize == 0) {
-			cout << "현재 등록된 계좌 정보가 없습니다." << endl;
-			return;
-		}
-
-		find = findAccountIdx(id);
-		if (find == -1) {
-			cout << "해당 계좌 ID를 가진 계좌 정보를 찾을 수 없습니다." << endl;
-			return;
-		}
-
-		accounts[find].balance += balance;
-		cout << "입금이 완료되었습니다." << endl;
-
-		return;
-	case 3:
-		cout << "[출    금]" << endl;
-
-		cout << "계좌ID: ";
-		cin >> id;
-		cout << "출금액: ";
-		cin >> balance;
-
-		if (accountSize == 0) {
-			cout << "현재 등록된 계좌 정보가 없습니다." << endl;
-			return;
-		}
-
-		find = findAccountIdx(id);
-		if (find == -1) {
-			cout << "해당 계좌 ID를 가진 계좌 정보를 찾을 수 없습니다." << endl;
-			return;
-		}
-
-		if (accounts[find].balance < balance) {
-			cout << "잔액이 부족합니다." << endl;
-			return;
-		}
-
-		accounts[find].balance -= balance;
-		cout << "출금이 완료되었습니다." << endl;
-
-		return;
-	case 4:
-		if (accountSize == 0) {
-			cout << "현재 등록된 계좌 정보가 없습니다." << endl;
-			return;
-		}
-
-		for (int i = 0; i < accountSize; i++) {
-			cout << "계좌ID: " << accounts[i].accId << endl;
-			cout << "이  름: " << accounts[i].cusName << endl;
-			cout << "잔  액: " << accounts[i].balance << endl;
-
-			if (i < accountSize - 1)
-				cout << endl;
-		}
-
-		return;
-	default:
-		cout << "올바르지 않은 번호입니다." << endl;
+	if (accountSize == ACCOUNT_MAX) {
+		cout << "계좌가 이미 " << ACCOUNT_MAX << "개입니다." << endl;
 		return;
 	}
+
+	find = findAccountIdx(id);
+	if (find != -1) {
+		cout << "이미 동일한 계좌 번호를 가진 계좌가 존재합니다." << endl;
+		return;
+	}
+
+	if (balance < 10) {
+		cout << "계좌 생성 시 최소 10원 이상의 돈을 입금해야합니다." << endl;
+		return;
+	}
+
+	accounts[accountSize].accId = id;
+	accounts[accountSize].balance = balance;
+	strcpy(accounts[accountSize++].cusName, name);
+
+	return;
+}
+
+void deposit() {
+	int balance;
+	int id, find;
+
+	cout << "계좌ID: ";
+	cin >> id;
+	cout << "입금액: ";
+	cin >> balance;
+
+	if (accountSize == 0) {
+		cout << "현재 등록된 계좌 정보가 없습니다." << endl;
+		return;
+	}
+
+	find = findAccountIdx(id);
+	if (find == -1) {
+		cout << "해당 계좌 ID를 가진 계좌 정보를 찾을 수 없습니다." << endl;
+		return;
+	}
+
+	accounts[find].balance += balance;
+	cout << "입금이 완료되었습니다." << endl;
+}
+
+void withdraw() {
+	int balance;
+	int id, find;
+
+	cout << "계좌ID: ";
+	cin >> id;
+	cout << "출금액: ";
+	cin >> balance;
+
+	if (accountSize == 0) {
+		cout << "현재 등록된 계좌 정보가 없습니다." << endl;
+		return;
+	}
+
+	find = findAccountIdx(id);
+	if (find == -1) {
+		cout << "해당 계좌 ID를 가진 계좌 정보를 찾을 수 없습니다." << endl;
+		return;
+	}
+
+	if (accounts[find].balance < balance) {
+		cout << "잔액이 부족합니다." << endl;
+		return;
+	}
+
+	accounts[find].balance -= balance;
+	cout << "출금이 완료되었습니다." << endl;
+}
+
+void seeList() {
+	if (accountSize == 0) {
+		cout << "현재 등록된 계좌 정보가 없습니다." << endl;
+		return;
+	}
+
+	for (int i = 0; i < accountSize; i++) {
+		cout << "계좌ID: " << accounts[i].accId << endl;
+		cout << "이  름: " << accounts[i].cusName << endl;
+		cout << "잔  액: " << accounts[i].balance << endl;
+
+		if (i < accountSize - 1)
+			cout << endl;
+	}
+}
+
+void duty(int input) {
+	cout << endl;
+
+	switch (input) {
+	case MK_ACC:
+		cout << "[계좌개설]" << endl;
+		mkAcc();
+
+		break;
+
+	case DEPOSIT:
+		cout << "[입    금]" << endl;
+		deposit();
+
+		break;
+
+	case WITHDRAW:
+		cout << "[출    금]" << endl;
+		withdraw();
+
+		break;
+
+	case SEE_LIST:
+		seeList();
+
+		break;
+
+	default:
+		cout << "올바르지 않은 번호입니다." << endl;
+		break;
+	}
+
+	cout << endl;
+	return;
 }
 
 int main() {
@@ -149,8 +187,6 @@ int main() {
 		if (input == 5)
 			return 0;
 
-		cout << endl;
 		duty(input);
-		cout << endl;
 	}
 }
